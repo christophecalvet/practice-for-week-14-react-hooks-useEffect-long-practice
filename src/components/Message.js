13d3cad10f0e7fb4742d73aca03c73fcbf083578
Christophe Calvet <onlyatest@mytest.com>
2024-09-17 16:36:39 +0200

    without bonus

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,45 @@
-function Message({ size }) {
-  console.log('Message', size);
+import { useEffect,useState } from 'react';
+
+function Message({ size,featherCount }) {
+
+
+  const [sizeClass, setSizeClass] = useState('');
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    console.log('PictureDisplay size', size);
+    let cname = '';
+    switch (size) {
+        case 'm':
+            cname = 'medium';
+            break;
+        case 'l':
+            cname = 'large';
+            break;
+        case 'xl':
+            cname = 'xlarge';
+            break;
+        default:
+            cname = 'small';
+            break;
+    }
+      // console.log(cname);
+      setSizeClass(cname);
+}, [size]);
+
+useEffect(() =>{
+  if (featherCount <= 0)
+      setMessage('Oh my! Your bird is naked!');
+  else if (featherCount >= 10) {
+      setMessage('Full turkey!');
+  } else {
+      setMessage('Coming along...');
+  }
+}, [featherCount])
 
   return (
-    <div className="message medium">
-      (Oh my! Your bird is naked!)
+    <div className={`message ${sizeClass}`}>
+      {message}
     </div>
   );
 };
